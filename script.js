b48c8d38b94b2938626ce8233cd355610ad032c9
Tim Oh <43765806+franciscotimoh@users.noreply.github.com>
2025-07-08 13:23:23 +0900

    Add logic for computer choosing a move

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Hello, World");
+// function `getComputerChoice`
+// randomly return one of the three Strings: "rock", "paper", "scissors"
+
+function getComputerChoice() {
+    let random = Math.floor(Math.random() * 3) + 1;
+    if (random === 1) {
+        return "Rock";
+    } else if (random === 2) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+
+console.log(getComputerChoice());
+
+// function `getHumanChoice`
+// prompt the user for one of the three Strings mentioned above.
+
+// keep track of player scores
+
+// create a function that plays a round
+
+// play 5 rounds
